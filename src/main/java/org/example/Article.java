7cca5e09b104f0609f067adc6bd6a66451a7ce45
r-uwu <yarrow.uwu@gmail.com>
2025-12-12 16:50:37 +0900

    DB에article INSERT, SELECT

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -5,12 +5,34 @@ public class Article {
     private int id;
     private String title;
     private String body;
+    private String regDate;
+    private String updateDate;
 
-    public Article(int id, String title, String body) {
+
+    public Article(int id, String regDate, String updateDate, String title, String body) {
         this.id = id;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
         this.title = title;
         this.body = body;
     }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
 
     public int getId() {
         return id;
--- src/main/java/org/example/JDBCConnectTest.java
+++ src/main/java/org/example/JDBCConnectTest.java
@@ -4,16 +4,41 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class JDBCConnectTest {
     public static void main(String[] args) {
         Connection conn = null;
+        PreparedStatement pstmt = null;
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
             conn = DriverManager.getConnection(url, "root", "");
             System.out.println("연결 성공!");
+
+            String sql = "INSERT INTO article";
+            sql += " SET regDate = NOW(),";
+            sql += "updateDate = NOW(),";
+            sql += "title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),";
+            sql += "`body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));";
+
+            System.out.println(sql);
+
+            pstmt = conn.prepareStatement(sql);
+
+            int affectedRow =  pstmt.executeUpdate();
+
+            System.out.println("affectedRow = " + affectedRow);
+
+
+           sql = "SELECT * FROM article;";
+           pstmt = conn.prepareStatement(sql);
+
+           //Araticle article = new Aritlcle(sql);
+
+           System.out.print(pstmt);
+
 
         } catch (ClassNotFoundException e) {
             System.out.println("드라이버 로딩 실패" + e);
